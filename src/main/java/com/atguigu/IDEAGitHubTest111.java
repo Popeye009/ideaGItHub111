d7a1c58ff78830c26f5db7aa14220def7bd314c9
wp <wp4751@163.com>
2019-10-23 12:52:34 +0800

    加了github1

--- src/main/java/com/atguigu/IDEAGitHubTest111.java
+++ src/main/java/com/atguigu/IDEAGitHubTest111.java
@@ -4,5 +4,6 @@ public class IDEAGitHubTest111 {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,github1");
+        System.out.println('A');
     }
 }
