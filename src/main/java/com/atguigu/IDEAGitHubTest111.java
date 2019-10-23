51d8adb473001ee0b8c2c2cd3681c8d63865303b
wp <wp4751@163.com>
2019-10-23 12:57:31 +0800

    111加入B

--- src/main/java/com/atguigu/IDEAGitHubTest111.java
+++ src/main/java/com/atguigu/IDEAGitHubTest111.java
@@ -5,6 +5,6 @@ public class IDEAGitHubTest111 {
         System.out.println("hello,git");
         System.out.println("hello,github1");
         System.out.println('A');
-        System.out.println('');B
+        System.out.println('B');
     }
 }
